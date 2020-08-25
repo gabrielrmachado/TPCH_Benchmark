08df197dd5b3d913d1f852dd6fb177d9edf80bae
Gabriel <gabriel.rmachado10@gmail.com>
2020-08-25 13:14:49 -0300

    DBGEN data generator included.

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     mysql = myclient.MySQL_TPCH("localhost", "user", "123456", "tpch100mb")
     benchmark = tpch.Benchmark(mysql, 0.1)
     benchmark.load_benchmark()
-    benchmark.power_benchmark()
+    # benchmark.power_benchmark()
